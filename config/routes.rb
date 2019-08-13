2dceb582d7d435fa61d920d6126fb2deb132a017
Alan Quinn <alancquinn2@gmail.com>
2019-08-13 15:44:11 +0100

    refactor: routes to resources, repeating form to _form partial, use before_action in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'tasks/new', to: 'tasks#new', as: :new
-  post 'tasks', to: 'tasks#create', as: :create
+  resources :tasks
 
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/new', to: 'tasks#new', as: :new
+  # post 'tasks', to: 'tasks#create', as: :create
 
-  # create
-  # update
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update', as: :update
-  #delete
-  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # create
+  # # update
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update', as: :update
+  # #delete
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 end
